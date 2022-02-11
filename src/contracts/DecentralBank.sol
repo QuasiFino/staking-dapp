b027e95d4a1905958cf07d8d6e16368e9dbb9022
wink-aajlak <aajlak.wink@gmail.com>
2022-02-11 08:00:59 +0530

    setting up the contract basics

--- src/contracts/DecentralBank.sol
+++ src/contracts/DecentralBank.sol
@@ -1,7 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity '0.8.0';
+import "./RWD.sol";
+import "./Tether.sol";
+
 
 contract DecentralBank {
   string public name = 'Decentral Bank';
   address public owner;
+  RWD public rwd;
+  Tether public tether;
+
+  constructor(RWD _rwd, Tether _tether) {
+    rwd = RWD(_rwd);
+    tether = Tether(_tether);
+  }
 }
